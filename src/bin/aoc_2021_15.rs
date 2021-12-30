67ff86e97f44582771498e131062a73fbd6406fe
Mautier <mau@tier>
2021-12-30 22:49:08 +0200

    2021-12-15: solution

---
+++ src/bin/aoc_2021_15.rs
@@ -0,0 +1,182 @@
+fn main() {
+    for test_file in ["2021-12-15.sample.txt", "2021-12-15.txt"] {
+        println!("--------------- {} ---------------", test_file);
+        let input_path = advent_of_code::env::get_puzzle_input_path(test_file);
+        let image = parse_input_image(advent_of_code::iter::line_iter_from_file(&input_path));
+
+        let optimal_path_cost = find_optimal_path(&image);
+        println!("Part 1: optimal path cost: {}", optimal_path_cost);
+
+        let tiled_image = expand_tile_into_full_image(&image);
+        let optimal_path_cost = find_optimal_path(&tiled_image);
+        println!("Part 2: optimal path cost: {}", optimal_path_cost);
+    }
+}
+
+struct Image<T> {
+    height: u16,
+    width: u16,
+    /// Linear buffer of pixels, in row-major order.
+    data: Vec<T>,
+}
+
+impl<T> Image<T> {
+    fn pixel(&self, row: u16, col: u16) -> &T {
+        &self.data[row as usize * self.width as usize + col as usize]
+    }
+    fn pixel_mut(&mut self, row: u16, col: u16) -> &mut T {
+        &mut self.data[row as usize * self.width as usize + col as usize]
+    }
+}
+
+/// Search for a minimal cost path between the top-left corner and the bottom right corner.
+///
+/// Uses an implementation of A* (https://en.wikipedia.org/wiki/A*_search_algorithm).
+fn find_optimal_path(image: &Image<u8>) -> usize {
+    // A heuristic that estimates the distance to the bottom right.
+    // For A* to find the optimal path first, this heuristic must never over-estimate the
+    // distance (it must be an admissible heuristic).
+    // To do so, we simply assume that all costs till the end are 1.
+    let estimated_dist_to_end = |row: u16, col: u16| -> usize {
+        (image.height - 1 - row) as usize + (image.width - 1 - col) as usize
+    };
+
+    // Using the heuristic "distance to the end", compute the heuristic "total cost".
+    let estimated_total_cost = |cost_so_far: usize, row: u16, col: u16| -> usize {
+        let est_remaining_cost = estimated_dist_to_end(row, col);
+        cost_so_far + est_remaining_cost
+    };
+
+    // The image of visited pixels.
+    let mut visited = Image {
+        height: image.height,
+        width: image.width,
+        data: vec![false; image.height as usize * image.width as usize],
+    };
+
+    // The open set of points, ie the pixels we haven't visited and could visit in the next step,
+    // starting from a previously visited pixel.
+    // This is a max heap, meaning max-cost first. To flip this we'll use Reverse.
+    use std::cmp::Reverse;
+    let mut to_visit = std::collections::BinaryHeap::new();
+    to_visit.push(Reverse(VisitCandidate {
+        row: 0,
+        col: 0,
+        cost_so_far: 0,
+        estimated_total_cost: estimated_total_cost(0, 0, 0),
+    }));
+
+    // Scratch buffer used for iterating over the valid neighbors. Cleared on every iteration.
+    let mut neighbors = Vec::with_capacity(4);
+
+    while let Some(cand) = to_visit.pop() {
+        let cand = cand.0; // Peel the Reverse.
+
+        if cand.row == image.height - 1 && cand.col == image.width - 1 {
+            return cand.cost_so_far;
+        }
+        *visited.pixel_mut(cand.row, cand.col) = true;
+
+        // Add all 4 neighbors to the open set.
+        neighbors.clear();
+        if 0 < cand.row && !visited.pixel(cand.row - 1, cand.col) {
+            neighbors.push((cand.row - 1, cand.col));
+        }
+        if cand.row + 1 < image.height && !visited.pixel(cand.row + 1, cand.col) {
+            neighbors.push((cand.row + 1, cand.col));
+        }
+        if 0 < cand.col && !visited.pixel(cand.row, cand.col - 1) {
+            neighbors.push((cand.row, cand.col - 1));
+        }
+        if cand.col + 1 < image.width && !visited.pixel(cand.row, cand.col + 1) {
+            neighbors.push((cand.row, cand.col + 1));
+        }
+
+        for &(r, c) in &neighbors {
+            let cost_so_far = cand.cost_so_far + *image.pixel(r, c) as usize;
+            to_visit.push(Reverse(VisitCandidate {
+                row: r,
+                col: c,
+                cost_so_far,
+                estimated_total_cost: estimated_total_cost(cost_so_far, r, c),
+            }));
+        }
+    }
+
+    panic!("BUG: should be unreachable");
+}
+
+#[derive(Clone, Debug, Eq)]
+struct VisitCandidate {
+    row: u16,
+    col: u16,
+    cost_so_far: usize,
+    estimated_total_cost: usize,
+}
+
+impl std::cmp::Ord for VisitCandidate {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.estimated_total_cost.cmp(&other.estimated_total_cost)
+    }
+}
+
+impl std::cmp::PartialOrd for VisitCandidate {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl std::cmp::PartialEq for VisitCandidate {
+    fn eq(&self, other: &Self) -> bool {
+        self.cost_so_far == other.cost_so_far
+    }
+}
+
+fn parse_input_image(lines: impl Iterator<Item = String>) -> Image<u8> {
+    let mut height = 0;
+    let mut width = 0;
+    let mut data = Vec::new();
+
+    for line in lines {
+        height += 1;
+        if width == 0 {
+            width = line.as_bytes().len() as u16;
+        }
+        assert_eq!(width, line.as_bytes().len() as u16);
+
+        data.extend(line.chars().map(|c| (c as u32 - '0' as u32) as u8));
+    }
+
+    Image {
+        height,
+        width,
+        data,
+    }
+}
+
+fn expand_tile_into_full_image(tile: &Image<u8>) -> Image<u8> {
+    let mut img = Image {
+        height: tile.height * 5,
+        width: tile.width * 5,
+        data: vec![0u8; tile.height as usize * 5 * tile.width as usize * 5],
+    };
+
+    for row_in_img in 0..img.height {
+        for col_in_img in 0..img.width {
+            let row_in_tile = row_in_img % tile.height;
+            let col_in_tile = col_in_img % tile.width;
+
+            let tile_index_rows = row_in_img / tile.height;
+            let tile_index_cols = col_in_img / tile.width;
+            let extra_risk = (tile_index_rows + tile_index_cols) as u8;
+
+            // This is a little more awkward than just "%10" because there is no risk level 0.
+            // So we remap the risk to [0,8], compute the extra risk %9, and then remap back to
+            // [1,9].
+            *img.pixel_mut(row_in_img, col_in_img) =
+                1 + (*tile.pixel(row_in_tile, col_in_tile) - 1 + extra_risk) % 9;
+        }
+    }
+
+    img
+}
